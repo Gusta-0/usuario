5dbf3aae577fb12c9d6a2ac7bd8e97224f3b73fd
Gustavo Alves <gabs.principal.2005@gmail.com>
2025-08-15 09:52:22 -0300

    adicionados metodos get, delete e login

--- src/main/java/com/gustavo/usuario/controller/UsuarioController.java
+++ src/main/java/com/gustavo/usuario/controller/UsuarioController.java
@@ -1,30 +1,30 @@
 package com.gustavo.usuario.controller;
 
 import com.gustavo.usuario.business.UsuarioService;
+import com.gustavo.usuario.infrastructure.dto.EnderecoDTO;
+import com.gustavo.usuario.infrastructure.dto.TelefoneDTO;
 import com.gustavo.usuario.infrastructure.dto.UsuarioDTO;
+import com.gustavo.usuario.infrastructure.entity.Usuario;
 import com.gustavo.usuario.infrastructure.security.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
+@RequiredArgsConstructor
+//@Tag(name = "Tarefas", description = "Cadastra tarefas de usuários")
+//@SecurityRequirement(name = SecurityConfig.SECURITY_SCHEME)
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
-
-    public UsuarioController(UsuarioService usuarioService, AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-        this.usuarioService = usuarioService;
-        this.authenticationManager = authenticationManager;
-        this.jwtUtil = jwtUtil;
-    }
 
     @PostMapping("/login")
     public String login(@RequestBody UsuarioDTO usuarioDTO) {
@@ -35,28 +35,55 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public ResponseEntity<UsuarioDTO> criar(@RequestBody UsuarioDTO usuarioDTO) {
-        return ResponseEntity.ok(usuarioService.salvarUsuario(usuarioDTO));
+    public ResponseEntity<UsuarioDTO> salvaUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return ResponseEntity.ok(usuarioService.salvaUsuario(usuarioDTO));
     }
-//
-//    @GetMapping
-//    public ResponseEntity<List<UsuarioResponse>> listarTodos() {
-//        return ResponseEntity.ok(usuarioService.listarTodos());
+
+    @GetMapping
+    public ResponseEntity<UsuarioDTO> buscaUsuarioPorEmail(@RequestParam("email") String email) {
+        return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
+    }
+
+//    @DeleteMapping("/{email}")
+//    public ResponseEntity<Void> deletaUsuarioPorEmail(@PathVariable String email) {
+//        usuarioService.deletaUsuarioPorEmail(email);
+//        return ResponseEntity.ok().build();
+//    }
+
+//    @PutMapping
+//    public ResponseEntity<UsuarioDTO> atualizDadoUsuario(@RequestBody UsuarioDTO dto,
+//                                                         @RequestHeader("Authorization") String token) {
+//        return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token, dto));
+//    }
+
+    @PutMapping("/endereco")
+    public ResponseEntity<EnderecoDTO> atualizaEndereco(@RequestBody EnderecoDTO dto,
+                                                        @RequestParam("id") Long id){
+        return ResponseEntity.ok(usuarioService.atualizaEndereco(id, dto));
+    }
+
+    @PutMapping("/telefone")
+    public ResponseEntity<TelefoneDTO> atualizaTelefone(@RequestBody TelefoneDTO dto,
+                                                        @RequestParam("id") Long id){
+        return ResponseEntity.ok(usuarioService.atualizaTelefone(id, dto));
+    }
+
+//    @PostMapping("/endereco")
+//    public ResponseEntity<EnderecoDTO> cadastaEndereco(@RequestBody EnderecoDTO dto,
+//                                                       @RequestHeader("Authorization") String token){
+//        return ResponseEntity.ok(usuarioService.cadastraEndereco(token, dto));
+//    }
+
+//    @PostMapping("/telefone")
+//    public ResponseEntity<TelefoneDTO> cadastraTelefone(@RequestBody TelefoneDTO dto,
+//                                                        @RequestHeader("Authorization") String token){
+//        return ResponseEntity.ok(usuarioService.cadastraTelefone(token, dto));
 //    }
 //
-//    @GetMapping("/{id}")
-//    public ResponseEntity<UsuarioResponse> buscarPorId(@PathVariable Long id) {
-//        return ResponseEntity.ok(usuarioService.buscarPorId(id));
+//    @GetMapping("/endereco/{cep}")
+//    public ResponseEntity<ViaCepDTO> buscarDadosCep(@PathVariable("cep") String cep){
+//        return ResponseEntity.ok(viaCepService.buscarDadosEndereco(cep));
 //    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<UsuarioResponse> atualizar(@PathVariable Long id, @RequestBody UsuarioRequest request) {
-//        return ResponseEntity.ok(usuarioService.atualizar(id, request));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deletar(@PathVariable Long id) {
-//        usuarioService.deletar(id);
-//        return ResponseEntity.noContent().build();
-//    }
+
+
 }
